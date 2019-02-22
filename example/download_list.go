20f2455c440ebeef6f717cdbe0b4c1e367fd5236
gswly <46489434+gswly@users.noreply.github.com>
2019-02-22 13:59:23 +0100

    fixed display error in examples

--- example/download_list.go
+++ example/download_list.go
@@ -29,7 +29,7 @@ func main() {
 
     // download has finished
     client.OnDownloadSuccessful = func(d *dctk.Download) {
-        fmt.Println("downloaded: %d", len(d.Content()))
+        fmt.Printf("downloaded: %d\n", len(d.Content()))
         client.Terminate()
     }
 
