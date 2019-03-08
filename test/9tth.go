b28e81257fc2cb0abf2c45ac66a2350d77965b8f
gswly <46489434+gswly@users.noreply.github.com>
2019-03-08 14:55:34 +0100

    test fixed

--- test/9tth.go
+++ test/9tth.go
@@ -13,32 +13,32 @@ func main() {
 	// http://adc.sourceforge.net/draft-jchapweske-thex-02.html
 
 	hash := dctk.TTHFromBytes([]byte{})
-	if hash != "LWPNACQDBZRYXW3VHJVCJ64QBZNGHOHHHZWCLNQ" {
+	if hash != dctk.TTHMust("LWPNACQDBZRYXW3VHJVCJ64QBZNGHOHHHZWCLNQ") {
 		panic(fmt.Errorf("wrong hash (3): %s", hash))
 	}
 
 	hash = dctk.TTHFromBytes([]byte("\x00"))
-	if hash != "VK54ZIEEVTWNAUI5D5RDFIL37LX2IQNSTAXFKSA" {
+	if hash != dctk.TTHMust("VK54ZIEEVTWNAUI5D5RDFIL37LX2IQNSTAXFKSA") {
 		panic(fmt.Errorf("wrong hash (4): %s", hash))
 	}
 
 	hash = dctk.TTHFromBytes([]byte(strings.Repeat("A", 1024)))
-	if hash != "L66Q4YVNAFWVS23X2HJIRA5ZJ7WXR3F26RSASFA" {
+	if hash != dctk.TTHMust("L66Q4YVNAFWVS23X2HJIRA5ZJ7WXR3F26RSASFA") {
 		panic(fmt.Errorf("wrong hash (5): %s", hash))
 	}
 
 	hash = dctk.TTHFromBytes([]byte(strings.Repeat("A", 1025)))
-	if hash != "PZMRYHGY6LTBEH63ZWAHDORHSYTLO4LEFUIKHWY" {
+	if hash != dctk.TTHMust("PZMRYHGY6LTBEH63ZWAHDORHSYTLO4LEFUIKHWY") {
 		panic(fmt.Errorf("wrong hash (6): %s", hash))
 	}
 
 	hash = dctk.TTHFromBytes([]byte(strings.Repeat("A", 10000)))
-	if hash != "UJUIOGYVALWRB56PRJEB6ZH3G4OLTELOEQ3UKMY" {
+	if hash != dctk.TTHMust("UJUIOGYVALWRB56PRJEB6ZH3G4OLTELOEQ3UKMY") {
 		panic(fmt.Errorf("wrong hash (7): %s", hash))
 	}
 
 	hash = dctk.TTHFromLeaves(dctk.TTHLeavesFromBytes([]byte(strings.Repeat("A", 10000))))
-	if hash != "UJUIOGYVALWRB56PRJEB6ZH3G4OLTELOEQ3UKMY" {
+	if hash != dctk.TTHMust("UJUIOGYVALWRB56PRJEB6ZH3G4OLTELOEQ3UKMY") {
 		panic(fmt.Errorf("wrong hash (8): %s", hash))
 	}
 
