fc3e8388bdf22d57ef0d1b84c04c6f8db48c4201
gswly <46489434+gswly@users.noreply.github.com>
2019-03-14 14:02:32 +0100

    documentation updated

--- client.go
+++ client.go
@@ -1,7 +1,39 @@
-// Package dctoolkit implements the client part of the Direct Connect
-// peer-to-peer system (ADC and NMDC protocols) in the Go programming language.
-// It allows the creation of clients that can interact with hubs and other
-// clients, and can be used as backend to user interfaces or automatic bots.
+/*
+Package dctoolkit implements the client part of the Direct Connect
+peer-to-peer system (ADC and NMDC protocols) in the Go programming language.
+It allows the creation of clients that can interact with hubs and other
+clients, and can be used as backend to user interfaces or automatic bots.
+
+Basic example (more are available at https://github.com/gswly/dctoolkit/tree/master/example)
+
+  package main
+
+  import (
+  	"fmt"
+  	dctk "github.com/gswly/dctoolkit"
+  )
+
+  func main() {
+  	// connect to hub in active mode. ports must be opened and accessible.
+  	client, err := dctk.NewClient(dctk.ClientConf{
+  		HubUrl:     "nmdc://hubip:411",
+  		Nick:       "mynick",
+  		TcpPort:    3009,
+  		UdpPort:    3009,
+  		TcpTlsPort: 3010,
+  	})
+  	if err != nil {
+  		panic(err)
+  	}
+
+  	// we are connected to the hub
+  	client.OnHubConnected = func() {
+  		fmt.Println("connected to hub")
+  	}
+
+  	client.Run()
+  }
+*/
 package dctoolkit
 
 import (
