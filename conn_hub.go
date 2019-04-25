43c0d5aa1cdc30ff6a0327095ed401528270d02e
gswly <46489434+gswly@users.noreply.github.com>
2019-04-25 14:06:36 +0200

    move hub keep aliver in separate file

--- conn_hub.go
+++ conn_hub.go
@@ -8,50 +8,6 @@ import (
 	"time"
 )
 
-const (
-	_HUB_KEEPALIVE_PERIOD = 120 * time.Second
-)
-
-type hubKeepAliver struct {
-	terminate chan struct{}
-	done      chan struct{}
-}
-
-func newHubKeepAliver(h *connHub) *hubKeepAliver {
-	ka := &hubKeepAliver{
-		terminate: make(chan struct{}, 1),
-		done:      make(chan struct{}),
-	}
-
-	go func() {
-		defer func() { ka.done <- struct{}{} }()
-
-		ticker := time.NewTicker(_HUB_KEEPALIVE_PERIOD)
-		defer ticker.Stop()
-		for {
-			select {
-			case <-ticker.C:
-				// we must call Safe() since conn.Write() is not thread safe
-				h.client.Safe(func() {
-					if h.client.protoIsAdc == true {
-						// ADC uses the TCP keepalive feature or empty packets
-					} else {
-						h.conn.Write(&msgNmdcKeepAlive{})
-					}
-				})
-			case <-ka.terminate:
-				return
-			}
-		}
-	}()
-	return ka
-}
-
-func (ka *hubKeepAliver) Close() {
-	ka.terminate <- struct{}{}
-	<-ka.done
-}
-
 type connHub struct {
 	client             *Client
 	terminateRequested bool
